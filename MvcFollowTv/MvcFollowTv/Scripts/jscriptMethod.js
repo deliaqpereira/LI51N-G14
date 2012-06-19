7c85eea5c72eb483af8c2a065d6d665d3febdb31
Delia Pereira <deliaqpereira@gmail.com>
2012-06-20 00:52:02 +0100

    Ordenação das colunas na paginação

--- MvcFollowTv/MvcFollowTv/Scripts/jscriptMethod.js
+++ MvcFollowTv/MvcFollowTv/Scripts/jscriptMethod.js
@@ -1,6 +1,5 @@
 ﻿function getPage(size)
 {
-    alert("getpage");
     window.pagesize = size;
     GetPageAjax(0);
 };
@@ -90,3 +89,55 @@ function GetPageAjax(pageBtn)
 
 
 };
+
+var asc = true;
+function ordena(col) 
+{
+    var rows = document.getElementsByClassName('items');
+    var valor = new Array();
+    var no;
+    
+    for (no = 0; no < rows.length; no++) {
+        var obj = new Object();
+        if (col == 0) {
+            obj.orderby = rows[no].getElementsByTagName('TD')[col].getElementsByTagName("A")[0].innerHTML.toLowerCase();
+        } else {
+            obj.orderby = rows[no].getElementsByTagName('TD')[col].innerHTML.toLowerCase();
+        }
+        obj.string = rows[no].innerHTML;
+        valor[no] = obj;
+    }
+
+    if (asc)
+    {
+        alert("asc");
+        valor = valor.sort(AscendentSort("orderby"));
+    }
+    else {
+
+        alert("desc");
+        valor = valor.sort(DescendentSort("orderby"));
+    }
+
+    if (asc == true)
+        asc = false;
+    else
+        asc = true;
+
+    for (no = 0; no < rows.length; no++) {
+        rows[no].innerHTML = valor[no].string;
+    }
+}
+
+function AscendentSort(property)
+{
+    return function (a, b) {
+        return (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    };
+}
+
+function DescendentSort(property) {
+    return function (a, b) {
+        return (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+    };
+}
